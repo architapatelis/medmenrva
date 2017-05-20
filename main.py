fcc10c30ae33f2f98b0d4a641da02c72a1f222f8
Archita Patel <architapatelis@gmail.com>
2017-05-20 15:29:57 -0700

    check to see if user is logged into gmail account

--- main.py
+++ main.py
@@ -11,34 +11,63 @@ environment = jinja2.Environment(loader = jinja2.FileSystemLoader(templates_fold
 
 class Handler(webapp2.RequestHandler):
     """Used to pass templates to each class"""
-    
+    def __init__(self, request=None, response=None):
+        super(Handler, self).__init__(request, response)
+        self.values = {}
+        self.get_user_info()
+        
     def write(self, *a):
         """ outputs the passed argument"""
         self.response.out.write(*a)
     
-    def render_html(self, template, params):
+    def render_html(self, template):
         html = environment.get_template(template)
-        return html.render(params)
+        return html.render(self.values)
     
-    def display_html(self, template, values):
-        self.write(self.render_html(template, values))
+    def display_html(self, template):
+        self.write(self.render_html(template))
+        
+    def get_user_info(self):
+        """
+        Figures out if the user is logged in and fills in global variables
+        if it is. If logged in, figures out if the user is signed up and 
+        sets variables for user information.
+        """
+        self.user = users.get_current_user()
+        if self.user:
+            url_link = users.create_logout_url("/")
+            email = self.user.email()
+        else:
+            url_link = users.create_login_url(self.request.uri)
+            
+        
+        self.values["url_link"] = url_link
+        self.values["user"] = self.user
+    
+    
+    def is_user_signed_up(self):
+        """
+        Returns true/false whether or not the user has signed up for medmenrva
+        """
+        pass
+    
+
+    def is_user_logged_in(self):
+        """
+        Returns true/false whether or not the user is signed in or not
+        """
+        if self.user:
+            return True
+        else:
+            return False
         
 
 class MainPage(Handler):
     
     def get(self):
+        self.get_user_info()
         
-        user = users.get_current_user()
-        if user:
-            url_link = users.create_logout_url("/")
-            email = user.email()
-        else:
-            url_link = users.create_login_url(self.request.uri)
-        values = {
-            "user": user,
-            "url_link": url_link
-            }
-        self.display_html("home.html", values)
+        self.display_html("home.html")
         
     
 
@@ -47,7 +76,7 @@ class SignUp(Handler):
     
     
     def get(self):
-        self.display_html("signup.html",{})
+        self.display_html("signup.html")
         
         
     def post(self):
@@ -62,7 +91,7 @@ class SignUp(Handler):
 class Login(Handler):
     
     def get(self):
-        self.display_html("login.html", {})
+        self.display_html("login.html")
         
 
 routes = [('/', MainPage),
